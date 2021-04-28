5b811f7e4c8ba5610997c812b1f0076868d2bad1
Uniguri <78708465+Uniguri@users.noreply.github.com>
2021-04-28 19:17:08 +0900

    Almost Compelete
    Almost Compelete

--- js/Linear_Regression.js
+++ js/Linear_Regression.js
@@ -1,6 +1,42 @@
+const learning_rate = 0.1;
+const epochs = 1000;
+
 class Data {
-    constructor(x, y){
+    constructor(x, y) {
         this.x = x;
         this.y = y;
     }
+}
+
+class Hypothesis {
+    constructor() {
+        this.w = Math.random()
+        this.b = Math.random()
+    }
+
+    H(x) {
+        return this.w * x + this.b
+    }
+}
+
+let hypothesis = new Hypothesis();
+
+function Regression(DataSet, size) {
+    let h = 0;
+    let cost = 0;
+    let gradient_w = 0;
+    let gradient_b = 0;
+
+    for (epoch = epochs; epoch > 0; epoch--) {
+        gradient_w = 0;
+        gradient_b = 0;
+        DataSet.forEach(e => {
+            gradient_w += ((-2) * e.x * (hypothesis.w * e.x + hypothesis.b - e.y)) / (size);
+            gradient_b += ((-2) * (hypothesis.w * e.x + hypothesis.b - e.y)) / (size);
+
+        });
+        hypothesis.w += learning_rate * gradient_w;
+        hypothesis.b += learning_rate * gradient_b;
+    }
+
 }
--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
-const leftmargin = 50;
-const bottommargin = 50;
-const offsetX = 30;
-const offsetY = 30;
+const margin = 50;
+const offset = 30;
 let DataSet = new Array();
 
-function init(){
+function init() {
     const canvas = document.createElement('canvas');
     canvas.id = "canvas";
     canvas.width = window.innerWidth;
@@ -17,32 +15,32 @@ function init(){
     mainDraw()
 }
 
-function mainDraw(){
+function mainDraw() {
     const canvas = document.getElementById('canvas');
     /** @type {CanvasRenderingContext2D} */
     const ctx = canvas.getContext('2d');
     const width = canvas.width;
     const height = canvas.height;
-    
+
     ctx.beginPath();
-    ctx.moveTo(leftmargin, 0);
-    ctx.lineTo(leftmargin, height - bottommargin);
-    ctx.moveTo(leftmargin, height- bottommargin);
-    ctx.lineTo(width, height - bottommargin);
+    ctx.moveTo(margin, 0);
+    ctx.lineTo(margin, height - margin);
+    ctx.moveTo(margin, height - margin);
+    ctx.lineTo(width, height - margin);
     ctx.stroke();
 
-    for(let x = leftmargin + offsetX; x < width; x+=offsetX){
+    for (let x = margin + offset; x < width; x += offset) {
         ctx.beginPath();
         ctx.strokeStyle = 'rgba(0,0,0,0.5)';
         ctx.moveTo(x, 0);
-        ctx.lineTo(x, height - bottommargin);
+        ctx.lineTo(x, height - margin);
         ctx.stroke();
     }
 
-    for(let y = height - bottommargin; y > 0; y-=offsetY){
+    for (let y = height - margin; y > 0; y -= offset) {
         ctx.beginPath();
         ctx.strokeStyle = 'rgba(0,0,0,0.5)';
-        ctx.moveTo(leftmargin, y);
+        ctx.moveTo(margin, y);
         ctx.lineTo(width, y);
         ctx.stroke();
     }
@@ -50,16 +48,19 @@ function mainDraw(){
     DataSet.forEach(e => {
         DataDraw(e);
     });
+
+    if( DataSet.length >= 2)
+        DrawHypothesis(hypothesis);
 }
 
-function DataAdd(x, y){
+function DataAdd(x, y) {
     data = new Data(x, y);
     DataSet.push(data);
 
     return data;
 }
 
-function DataDraw(data){
+function DataDraw(data) {
     const canvas = document.getElementById('canvas');
     /** @type {CanvasRenderingContext2D} */
     const ctx = canvas.getContext('2d');
@@ -68,21 +69,43 @@ function DataDraw(data){
 
     ctx.beginPath();
     ctx.fillStyle = 'RED';
-    ctx.arc(data.x+leftmargin, window.innerHeight-data.y-bottommargin, 3, 0, 2*Math.PI);
+    ctx.arc(data.x*offset*offset + margin, window.innerHeight - data.y*offset*offset - margin, 3, 0, 2 * Math.PI);
     ctx.fill();
 }
 
+function DrawHypothesis(hypothesis){
+    const canvas = document.getElementById('canvas');
+    /** @type {CanvasRenderingContext2D} */
+    const ctx = canvas.getContext('2d');
+    const width = canvas.width;
+    const height = canvas.height;
+
+    ctx.beginPath();
+    ctx.moveTo(margin, height -margin - hypothesis.H( 0 ) * offset*offset );
+    ctx.lineTo(width, height -margin - hypothesis.H( (window.innerWidth-margin) / (offset*offset)) * offset*offset );
+    ctx.stroke();
+
+    ctx.clearRect(margin+1, height-margin+1, width, height);
+}
+
 window.addEventListener("click", (event) => {
-    if(event.x >= leftmargin && event.y <= window.innerHeight-bottommargin){
-        tempx = event.x-leftmargin;
-        tempy = window.innerHeight-event.y-bottommargin;
-        console.log(tempx,tempy);
+    if (event.x >= margin && event.y <= window.innerHeight - margin) {
+        tempx = (event.x - margin)/(offset*offset);
+        tempy = (window.innerHeight - event.y - margin)/(offset*offset);
         temp = DataAdd(tempx, tempy);
         DataDraw(temp);
+
+        if(DataSet.length >= 2){
+            Regression(DataSet, DataSet.length);
+
+            document.getElementById('canvas').remove();
+            init()
+        }
+
     }
 });
 
-window.onresize = function(){
+window.onresize = function () {
     document.getElementById('canvas').remove();
     init();
 };
